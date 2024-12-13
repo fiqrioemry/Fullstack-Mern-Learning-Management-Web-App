cf29cd142ea1bc50851ee97c00b678c50ada487a
fiqrioemry <foemry@gmail.com>
2024-12-13 22:11:03 +0700

    add progress controler

--- server/controllers/progress/index.js
+++ server/controllers/progress/index.js
@@ -1,3 +1,7 @@
+const markCurrentLectureAsViewed = require("markCurrentLectureAsViewed,");
+const getCurrentCourseProgress = require("getCurrentCourseProgress,");
+const resetCurrentCourseProgress = require("resetCurrentCourseProgress,");
+
 module.exports = {
   markCurrentLectureAsViewed,
   getCurrentCourseProgress,
